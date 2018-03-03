d82629ecf2a73ac88653dead6658a4e41226d5ab
michael bychkowski <mbychkowski@gmail.com>
2018-03-03 14:02:10 -0500

    api routes working. Issues with routing address.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const friends = require('../data/friend.js');
 
@@ -7,20 +8,18 @@ var routeAPI = express.Router();
 routeAPI.use(bodyParser.urlencoded({extended: false}));
 routeAPI.use(bodyParser.json());
 
-routeAPI.get('/api/friends', function(res, req) {
-  console.log(friends);
+routeAPI.get('/api/friends', function(req, res) {
+  res.json(friends);
+});
 
-  return res.json(friends);
+routeAPI.post('/api/friends', function(res, req) {
+  var newFriend = req.body;
+
+  console.log(newFriend);
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
 });
 
 module.exports = routeAPI;
-
-// app.post('/api/friends', function(res, req) {
-//   var newRequest = req.body;
-//
-//   console.log(newFriend);
-//
-//   friends.push(newFriend);
-//
-//   res.json(newFriend);
-// });
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
-const express = require('express');
+const express = require('express')
 
-const routeHTML = require('./app/routing/htmlRoutes');
-const routeAPI = require('./app/routing/apiRoutes');
+const routeHTML = require('./app/routing/htmlRoutes.js');
+const routeAPI = require('./app/routing/apiRoutes.js');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use('/', routeHTML);
 app.use('/survey', routeHTML);
-//
-var routeAPI = express.Router();
-
-routeAPI.use(bodyParser.urlencoded({extended: false}));
-routeAPI.use(bodyParser.json());
-
-routeAPI.get('/api/friends', function(res, req) {
-  console.log(friends);
-
-  return res.json(friends);
-});
-
 app.use('/api/friends', routeAPI);
-//
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
