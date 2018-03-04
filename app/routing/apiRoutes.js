9f179a5220afdff247311fe9b2ea30165f76f933
michael bychkowski <mbychkowski@gmail.com>
2018-03-04 09:18:22 -0500

    Ajax get request for matching algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,8 +15,6 @@ routeAPI.get('/api/friends', function(req, res) {
 routeAPI.post('/api/friends', function(req, res) {
   var newFriend = req.body;
 
-  console.log('New Friends:', newFriend);
-
   friends.push(newFriend);
 
   res.json(newFriend);
