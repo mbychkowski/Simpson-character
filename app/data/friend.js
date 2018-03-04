8bc48cac257b96a6f85ec8a01e6cc538639e7f98
michael bychkowski <mbychkowski@gmail.com>
2018-03-04 13:04:59 -0500

    update characters to be-friend

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,20 +1,38 @@
-var friends = [
-  {
+var friends = [{
     'name': 'Bart Simpson',
     'photo': 'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png',
-    'scores': ['5', '1', '4', '4', '5', '1', '2', '5', '4', '1']
+    'scores': ['5', '4', '5', '3', '2', '1', '5', '2', '3', '1']
   },
   {
     'name': 'Lisa Simpson',
-    'photo': 'https://365psd.com/images/previews/64b/lisa-simpson-1-46483.jpg',
-    'scores': ['1', '5', '2', '2', '1', '5', '4', '1', '2', '3']
+    'photo': 'https://vignette.wikia.nocookie.net/simpsons/images/1/12/Lisa_Simpson-0.png/revision/latest?cb=20161027220133',
+    'scores': ['5', '2', '5', '2', '4', '5', '1', '1', '4', '4']
   },
   {
     'name': 'Ned Flanders',
-    'photo': 'https://s-i.huffpost.com/gen/2951352/images/o-NED-FLANDERS-facebook.jpg',
-    'scores': ['2', '1', '5', '4', '2', '3', '2', '5', '5', '5']
+    'photo': 'http://simpsons.wikia.com/wiki/Ned_Flanders/Gallery',
+    'scores': ['5', '2', '5', '3', '4', '4', '2', '1', '5', '4']
   },
-
+  {
+    'name': 'Homer Simpson',
+    'photo': 'https://vignette.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png/revision/latest?cb=20091207194310',
+    'scores': ['5', '5', '5', '5', '2', '3', '4', '3', '1', '3']
+  },
+  {
+    'name': 'Ralph Wiggum',
+    'photo': 'https://vignette.wikia.nocookie.net/simpsons/images/1/14/Ralph_Wiggum.png/revision/latest?cb=20100704163100',
+    'scores': ['3', '3', '3', '3', '3', '3', '3', '5', '3', '3']
+  },
+  {
+    'name': 'Apu',
+    'photo': 'https://vignette.wikia.nocookie.net/simpsons/images/7/7d/Apu_Nahasapeemapetilon.png/revision/latest?cb=20171027210822',
+    'scores': ['5', '3', '5', '4', '5', '3', '2', '2', '4', '2']
+  },
+  {
+    'name': 'Maggie Simpson',
+    'photo': 'https://vignette.wikia.nocookie.net/simpsons/images/6/6c/MaggieSimpson.PNG/revision/latest?cb=20170101014602',
+    'scores': ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
+  }
 ];
 
 module.exports = friends;
