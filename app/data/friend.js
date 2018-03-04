4b8689bec0581381c31c47e3a7ff79567cb2aee2
michael bychkowski <mbychkowski@gmail.com>
2018-03-04 11:24:59 -0500

    friend matching algorith complete

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,9 +1,20 @@
 var friends = [
   {
-  name: 'Bart Simpson',
-  photo: 'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png',
-  scores: ['5', '1', '4', '4', '5', '1', '2', '5', '4', '1']
-  }
+    'name': 'Bart Simpson',
+    'photo': 'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png',
+    'scores': ['5', '1', '4', '4', '5', '1', '2', '5', '4', '1']
+  },
+  {
+    'name': 'Lisa Simpson',
+    'photo': 'https://365psd.com/images/previews/64b/lisa-simpson-1-46483.jpg',
+    'scores': ['1', '5', '2', '2', '1', '5', '4', '1', '2', '3']
+  },
+  {
+    'name': 'Ned Flanders',
+    'photo': 'https://s-i.huffpost.com/gen/2951352/images/o-NED-FLANDERS-facebook.jpg',
+    'scores': ['2', '1', '5', '4', '2', '3', '2', '5', '5', '5']
+  },
+
 ];
 
 module.exports = friends;
